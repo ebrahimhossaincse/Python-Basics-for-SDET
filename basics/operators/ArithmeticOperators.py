49c3638217eacf80f7ea4542d8d99ce586773aea
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2025-02-05 03:06:59 -0500

    add variables example

---
+++ basics/operators/ArithmeticOperators.py
@@ -0,0 +1,13 @@
+number_1 = 10
+number_2 = 5
+sum_result = number_1 + number_2  # Addition
+difference = number_1 - number_2  # Subtraction
+product = number_1 * number_2  # Multiplication
+quotient = number_1 / number_2  # Division
+remainder = number_1 % number_2  # Modulus
+
+print("Sum:", sum_result)
+print("Difference:", difference)
+print("Product:", product)
+print("Quotient:", quotient)
+print("Remainder:", remainder)
---
+++ basics/variables/Variables_And_DataTypes.py
@@ -0,0 +1,19 @@
+# Variables with different data types
+age = 26  # int
+height = 5.9  # float
+name = "Ebrahim"  # str
+is_adult = True  # bool
+grades = [90, 85, 92]  # list
+coordinates = (3, 7)  # tuple
+person = {"name": "Bob", "age": 30}  # dict
+unique_colors = {"red", "green", "blue"}  # set
+
+# Displaying values
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Adult?", is_adult)
+print("Grades:", grades)
+print("Coordinates:", coordinates)
+print("Person:", person)
+print("Unique Colors:", unique_colors)
